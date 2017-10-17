84efc0a4a5b17a6621d2bdb5aac981429a79aedc
Guest User <Guest@epicodus-10.local>
2017-10-17 16:17:04 -0700

    add jquery function to delete <li> once it is clicked on

--- js/scripts.js
+++ js/scripts.js
@@ -2,38 +2,39 @@ $(document).ready(function() {
   $("button#hello").click(function() {
     $("ul#user").prepend("<li>Hello!</li>");
     $("ul#webpage").prepend("<li>Why hello there!</li>");
-    $('li').click(function() {
-      $('li').css('background-color', 'green');
-      //$(this).remove();
+
+    $("ul#user").children("li").first().click(function() {
+      $(this).remove();
     });
-    // $("ul#user").children()("li").first().click(function() {
-    //   $(this).remove();
-    // });
-    // $("ul#webpage").children()("li").first().click(function() {
-    //   $(this).remove();
-    // });
+
+    $("ul#webpage").children("li").first().click(function() {
+        $(this).remove();
+    });
   });
 
   $("button#goodbye").click(function() {
     $("ul#user").prepend("<li>Goodbye!</li>");
     $("ul#webpage").prepend("<li>Goodbye, dear user!</li>");
-    // $("ul#user").children()("li").first().click(function() {
-    //   //$(this).remove();
-    //   alert('hi');
-    // });
-    // $("ul#webpage").children()("li").first().click(function() {
-    //   $(this).remove();
-    // });
+
+    $("ul#user").children("li").first().click(function() {
+      $(this).remove();
+    });
+
+    $("ul#webpage").children("li").first().click(function() {
+      $(this).remove();
+    });
   });
 
   $("button#stop").click(function() {
     $("ul#user").prepend("<li>Stop copying me!</li>");
     $("ul#webpage").prepend("<li>Pardon me. I meant no offense.</li>");
-    // $("ul#user").children()("li").first().click(function() {
-    //   $(this).remove();
-    // });
-    // $("ul#webpage").children()("li").first().click(function() {
-    //   $(this).remove();
-    // });
+
+    $("ul#user").children("li").first().click(function() {
+      $(this).remove();
+    });
+
+    $("ul#webpage").children("li").first().click(function() {
+      $(this).remove();
+    });
   });
 });
